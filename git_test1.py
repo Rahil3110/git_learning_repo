4673c2b2081cac2bfd17c86bdb373ef7e3bd4678
Rahil3110 <rahilalingal07@gmail.com>
2025-07-25 10:47:19 +0530

    updated and new file

--- git_test1.py
+++ git_test1.py
@@ -1,9 +1,10 @@
-n1=int(input("enter 1st number"))
-n2=int(input("enter 2nd number"))
-if n1>n2:
+n1 = int(input("Enter 1st number: "))
+n2 = int(input("Enter 2nd number: "))
+
+if n1 > n2:
     print("n1 is largest")
-elif:
+elif n2 > n1:
     print("n2 is largest")
 else:
-    print(" n1 and n2 are equal")
-    
+    print("n1 and n2 are equal")
+
